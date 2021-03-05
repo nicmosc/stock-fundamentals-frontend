6679eef5099c119973a835012df0899ddf5749ba
nicmosc <nicmosc@users.noreply.github.com>
2021-03-05 10:08:36 +0100

    add emotion css

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
-import './App.css';
+import { css } from '@emotion/css';
 
 import logo from './logo.svg';
 
+const styles = {
+  app: css`
+    text-align: center;
+  `,
+  logo: css`
+    height: 40vmin;
+    pointer-events: none;
+  `,
+  header: css`
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+  `,
+  link: css`
+    color: #61dafb;
+  `,
+};
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.app}>
+      <header className={styles.header}>
+        <img src={logo} className={styles.logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reloader
         </p>
         <a
-          className="App-link"
+          className={styles.link}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
