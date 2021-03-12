8b7e22b7aac10aa25555a9b66f0b5a82e340dd73
nicmosc <nicmosc@users.noreply.github.com>
2021-03-12 15:11:15 +0100

    split sort by

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,21 +1,18 @@
-import { EllipsisOutlined } from '@ant-design/icons';
 import { css, cx } from '@emotion/css';
-import { Col, Popover, Row } from 'antd';
+import { Col, Row } from 'antd';
 import { CSSProperties, Fragment, useRef, useState } from 'react';
 import AutoSizer from 'react-virtualized-auto-sizer';
 import { FixedSizeList as List } from 'react-window';
 
 import { Stock } from '../types';
 import { ANIMATION_CURVE, ANIMATION_TIME, Color, Size, getSectorColors, round } from '../utils';
-import { Box } from './Box';
 import { Discount } from './Discount';
+import { SortBy } from './SortBy';
 import { Text } from './Text';
 
 const styles = {
   panel: css`
     position: relative;
-    /* padding: ${Size.EXTRA_LARGE}px calc(${Size.EXTRA_LARGE}px + ${Size.MEDIUM}px); */
-    /* padding: ${Size.EXTRA_LARGE}px 0; */
     border-radius: ${Size.EXTRA_LARGE}px;
     border-bottom-right-radius: 0;
     border-bottom-left-radius: 0;
@@ -83,31 +80,7 @@ const styles = {
       cursor: grab;
     }
   `,
-  moreIcon: css`
-    color: ${Color.tertiary};
 
-    path {
-      stroke: ${Color.tertiary};
-      stroke-width: 70;
-    }
-
-    &:hover {
-      cursor: pointer;
-      color: ${Color.secondary};
-
-      path {
-        stroke: ${Color.secondary};
-      }
-    }
-  `,
-  option: css`
-    border-radius: ${Size.EXTRA_SMALL}px;
-
-    &:hover {
-      cursor: pointer;
-      background: ${Color.grey[2]};
-    }
-  `,
   table: css`
     padding-bottom: ${Size.EXTRA_LARGE * 3}px;
   `,
@@ -135,65 +108,6 @@ const styles = {
     right: ${Size.EXTRA_LARGE}px;
     z-index: 9;
   `,
-};
-
-const SortBy = () => {
-  return (
-    <Popover
-      placement="bottomRight"
-      arrowPointAtCenter
-      trigger="click"
-      overlayInnerStyle={{
-        color: Color.primary,
-        padding: 0,
-        borderRadius: Size.SMALL,
-      }}
-      content={
-        <div>
-          <Box size={{ bottom: Size.SMALL, left: Size.MEDIUM, right: Size.MEDIUM }}>
-            <Text color={Color.secondary}>Sort by</Text>
-          </Box>
-          <div className={styles.option}>
-            <Box
-              inset
-              size={{
-                right: Size.MEDIUM,
-                left: Size.MEDIUM,
-                bottom: Size.EXTRA_SMALL,
-                top: Size.EXTRA_SMALL,
-              }}>
-              <Text light>Alphabetical Order</Text>
-            </Box>
-          </div>
-          <div className={styles.option}>
-            <Box
-              inset
-              size={{
-                right: Size.MEDIUM,
-                left: Size.MEDIUM,
-                bottom: Size.EXTRA_SMALL,
-                top: Size.EXTRA_SMALL,
-              }}>
-              <Text light>Largest disconut</Text>
-            </Box>
-          </div>
-          <div className={styles.option}>
-            <Box
-              inset
-              size={{
-                right: Size.MEDIUM,
-                left: Size.MEDIUM,
-                bottom: Size.EXTRA_SMALL,
-                top: Size.EXTRA_SMALL,
-              }}>
-              <Text light>Fundamentals Score</Text>
-            </Box>
-          </div>
-        </div>
-      }>
-      <EllipsisOutlined className={styles.moreIcon} style={{ fontSize: Size.LARGE }} />
-    </Popover>
-  );
 };
 
 interface PanelProps {
